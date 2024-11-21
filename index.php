e2355d4754bf99375dd2a66505f306f5959e7f17
AdamBrown04 <adambrown2004@outlook.com>
2024-11-21 17:31:22 +0000

    started working on the layout of the index page, re-added bootstrap but a lighter version instead

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Country List</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet">
+<script src="bootstrap-5.3.3-dist/js/bootstrap.js" defer></script>
+<title>Country List</title>
 </head>
 
 <body>
-    <form class="form-inline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <div class="d-flex">
+        <div class="p-2 flex-fill"><input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></div>
+        <div class="p-2"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></div>
+        <div class="dropdown p-2">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  Select Year </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </div>
+    </div>
     <table class="table table-bordered">
         <thead>
             <tr>
