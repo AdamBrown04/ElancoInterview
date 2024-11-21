cbf34d92b533f47e82c6bb9b4df0b4fc1455a1e7
AdamBrown04 <adambrown2004@outlook.com>
2024-11-21 15:19:58 +0000

    removed Document as the title for countryDetails, replaced with place holder title for now. Removed bootstrap

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Country List</title>
 </head>
+
 <body>
-    
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">Flag</th>
+                <th scope="col">Country Name</th>
+                <th scope="col">Population</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
